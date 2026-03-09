27fb6d97868fdf312c4be111486eb83f54a7f3cc
cory-wood-hcp <cory.wood@hashicorp.com>
2026-03-09 14:15:37 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "operate_project_id" {
 output "sandbox_project_id" {
   value = tfe_project.wlz_aws_sandbox.id
 }
+
+output "iam_access_variable_set_id" {
+  value = tfe_variable_set.aws_iam_access.id
+}
